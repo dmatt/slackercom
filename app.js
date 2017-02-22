b7da81b9a7f4feee33475fedb254309fd9af4e71
System <none>
2017-02-22 06:19:06 +0000

    Checkpoint

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ var app = express()
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
-  console.log(req)
+  // if query.token = '2365Mb38QS6zo2E8azlirAwT'
+  // if no additional text
+  // func() CASE STATUS (all filters and #s)  
+  // if regex = case ID
+  // func() case body and link
+  // if regex = email address
+  // func() recent cases matching email
+  console.log(req);
 })
 
 app.listen(3000, function () {
