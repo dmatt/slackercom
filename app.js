5ab2f1c6f182e8def9feb5b4d30f573079701c28
System <none>
2017-02-24 06:04:51 +0000

    Checkpoint

--- app.js
+++ app.js
@@ -9,18 +9,24 @@ var desk = require('./my-desk').createClient({
 });
 
 app.get('/', function (req, res) {
-  // if query.token = '2365Mb38QS6zo2E8azlirAwT'
-  // if no additional text
-  // func() CASE STATUS (all filters and #s)  
-  // if regex = case ID
-  // func() case body and link
-  // if regex = email address
-  // func() recent cases matching email
-  desk.cases({status: 'new,open', priority:'9'}, function(error, data) {   
-    console.log(error);
-    console.log(data);
-    res.send(data);
-  });
+  if (req.query.token = '2365Mb38QS6zo2E8azlirAwT') {
+  // if (req.method === 'GET' && request.url === '/echo')
+    console.log(req);
+    res.send('hi slack');
+  }
+  else {
+    // if no additional text
+    // func() CASE STATUS (all filters and #s)  
+    // if regex = case ID
+    // func() case body and link
+    // if regex = email address
+    // func() recent cases matching email
+    desk.cases({status: 'new,open', priority:'9'}, function(error, data) { 
+      console.log(error);
+      console.log(data);
+      res.send(data);
+    });   
+  }
 })
 
 // How to build message response back to slack http://phabricator.local.disqus.net/diffusion/HUBOT/browse/master/scripts/embedcomment.coffee
