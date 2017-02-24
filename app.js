937950ea999e649bbe8300ea5a7606c9d6fc68f9
System <none>
2017-02-24 06:24:55 +0000

    Checkpoint

--- app.js
+++ app.js
@@ -8,24 +8,26 @@ var desk = require('./my-desk').createClient({
   token_secret: process.env.TOKEN_SECRET
 });
 
-app.get('/', function (req, res) {
-  if (req.query.token = '2365Mb38QS6zo2E8azlirAwT') {
-    console.log(req.query.token);
-    res.send('hi slack');
+app.post('/', function (req, res) {
+  if (req.query.token == '2365Mb38QS6zo2E8azlirAwT') {
+    desk.cases({status: 'new,open', priority:'9'}, function(error, data) {
+          console.log(req.query.token);
+          res.send('hi slack'+data);      
+        });
   }
   else {
+    res.send('hi non-disqus POST');    
     // if no additional text
     // func() CASE STATUS (all filters and #s)  
     // if regex = case ID
     // func() case body and link
     // if regex = email address
     // func() recent cases matching email
-    desk.cases({status: 'new,open', priority:'9'}, function(error, data) { 
-      console.log(error);
-      console.log(data);
-      res.send('hi web user'+data);
-    });   
   }
+})
+
+app.get('/', function (req, res) {
+      res.send('hi web user');
 })
 
 // How to build message response back to slack http://phabricator.local.disqus.net/diffusion/HUBOT/browse/master/scripts/embedcomment.coffee
