4b4305dbf64830288bb0860c4c4d47206179d507
System <none>
2017-03-05 02:00:33 +0000

    Checkpoint

--- app.js
+++ app.js
@@ -29,13 +29,13 @@ app.post('/', function (req, res) {
       var i = 1
       function deskCall() {
         desk.cases({labels:['Priority publisher,SaaS Ads,Direct publisher,Community publisher,Home,Community commenter'], status:['new,open'], sort_field:'created_at', sort_direction: 'asc', per_page:100, page:i}, function(error, data) {
-          if (data._links.next != null) {
+          if (i < 3) {
             console.log('next object: ',data._links.next)
             dataEntries = dataEntries.concat(data._embedded.entries)
             console.log(data._embedded.entries.length)            
             console.log('BEFORE passing in!',dataEntries.length)
-            deskCall()
             i++
+            deskCall()
           } else if (!data) {
             console.log(error)
           } else {
