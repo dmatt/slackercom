1bfbe9da5a04ce217e2bb02e3b5a0073c7ac785f
System <none>
2017-03-10 01:04:09 +0000

    Checkpoint

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ const bodyParser = require('body-parser')
 //   
 //   statusParams - Object of criteria for `cases` API call via deskCall(), parameters available: http://dev.desk.com/API/cases/#fields
 //   createStats() - Array.prototype.filter() tests to further segment data from deskCall() into specific filters
-//   stats - Object of filter names and variables defined in createStats() to output as Slack attachments.
+//   stats - Object of filter names and variables defined in createStats() to output up to 20 Slack attachments
 //
 //
 // Commands:
 //   /support - default command returns case status of all important Desk filters
-//   /support https://help.disqus.com/agent/case/347519 - returns case that matches ID provided
-//   /support archon@gmail.com - returns case that matches email provided
+//   /support https://help.disqus.com/agent/case/347519 - returns case that matches ID provided 0-7 characters in length
+//   /support example@gmail.com - returns most recent case that matches email provided
 //   /support help - returns help with commands
 
 const desk = require('./my-desk').createClient({
