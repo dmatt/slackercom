922e15633c79ce605b0b0adeff2d944a11001c32
System <none>
2017-06-09 18:18:01 +0000

    Checkpoint

--- app.js
+++ app.js
@@ -327,6 +327,7 @@ function status(res,type) {
       res.send(statusMessage);
     } else {
       webhook({text:"Morning report incoming!"});
+      res.send(statusMessage);
       webhook(statusMessage);
     }
     store(stats);
