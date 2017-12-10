57870c40bcea29b6ece71a7adb59add574f311a8
Glitch (tickets) <none>
2017-12-10 04:14:17 +0000

    🌋👨‍❤️‍💋‍👨 Checkpoint app.js:196725 .env:196725 package.json:196725

--- app.js
+++ app.js
@@ -13,10 +13,11 @@ const desk = require('./my-desk').createClient({
 var Twitter = require('twit'),
   config = { // Be sure to update the .env file with your API keys 
     twitter: {
-      consumer_key: process.env.CONSUMER_KEY,
-      consumer_secret: process.env.CONSUMER_SECRET,
-      access_token: process.env.ACCESS_TOKEN,
-      access_token_secret: process.env.ACCESS_TOKEN_SECRET
+      consumer_key: process.env.TWITTER_CONSUMER_KEY,
+      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+      access_token: process.env.TWITTER_ACCESS_TOKEN,
+      access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+      //timeout_ms: 60*1000
     }
   },
   T = new Twitter(config.twitter),
