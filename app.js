515666bc4f0c04334bf36d343c8ef36b85ec9ebf
Glitch (slackercom) <none>
2018-07-12 22:37:42 +0000

    🐯⛰ Checkpoint ./app.js:196725/166 ./package.json:196725/50

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const request = require('request')
 const Intercom = require('intercom-client');
 const mongojs = require('mongojs')
 
+var ip = require("ip");
+console.dir ( ip.address() );
+
 const client = new Intercom.Client({ token: process.env.INTERCOM_TOKEN });
 db = mongojs(`#{process.env.DB_USER}:#{process.env.DB_PASS}@#{process.env.DB_URL}`)
-
 
 // Use glitchup package to prevent server from sleeping
 const glitchup = require('glitchup');
