bd23dd6b81a7c04e1cde88c17ce9a8cd3dec5d02
dmatt <dmatt@disqus.com>
2018-09-20 16:59:48 -0700

    Start using eslint and fix syntax issues

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 // 👀 Current status: ne-db is working, need to design the records, mapStats gets total count
 
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 const Intercom = require('intercom-client');
-const client = new Intercom.Client({ token: process.env.INTERCOM_TOKEN });
 const glitchup = require('glitchup');
+const Datastore = require('nedb'); // setup a new database
+
+const app = express();
+const client = new Intercom.Client({ token: process.env.INTERCOM_TOKEN });
+const db = new Datastore({ filename: '.data/datafile', autoload: true });
 
 // Express middleware for parsing request/resonse bodies
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Important functions to re-write
 // status() - default
@@ -25,29 +28,20 @@ function StatusRecord(timestamp, data, total) {
   this.total = total;
 }
 
-let statusRecord1 = new StatusRecord("today", {"convo": "1"}, 1);
-let statusRecord2 = new StatusRecord("yesterday", {"convo": "2"}, 2);
-
 // default conversations status data
-var conversations = [
-  statusRecord1,
-  statusRecord2
+const defaultStatusRecords = [
+  new StatusRecord('today', { convo: '1' }, 1),
+  new StatusRecord('yesterday', { convo: '2' }, 2),
 ];
 
-// setup a new database
-var Datastore = require('nedb'),
-    // Security note: the database is saved to the file `datafile` on the local filesystem. It's deliberately placed in the `.data` directory
-    // which doesn't get copied if someone remixes the project.
-    db = new Datastore({ filename: '.data/datafile', autoload: true });
-
-db.count({}, function (err, count) {
-  console.log("There are " + count + " conversation rows in the database");
-  if(err) console.log("There's a problem with the database: ", err);
-  else if(count<=0){ // empty database so needs populating
+db.count({}, (countErr, count) => {
+  console.log(`There are ${count} conversation rows in the database`);
+  if (countErr) console.log('There\'s a problem with the database: ', countErr);
+  else if (count <= 0) { // empty database so needs populating
     // default users inserted in the database
-    db.insert(conversations, function (err, conversationsAdded) {
-      if(err) console.log("There's a problem with the database: ", err);
-      else if(conversationsAdded) console.log("Default conversationsAdded inserted in the database");
+    db.insert(defaultStatusRecords, (insertErr, recordsAdded) => {
+      if (insertErr) console.log('There\'s a problem with the database: ', insertErr);
+      else if (recordsAdded) console.log('Default recordsAdded inserted in the database');
     });
   }
 });
@@ -60,123 +54,125 @@ let monitoredTeams = []
 // setInterval(list, 3000 );
 
 // Call intercom for first page converations and paginate if more results exist
-function list() {
-  client.conversations.list( { open: true, per_page: 20 }).then(
-    function (firstPage, acc = []) {
-      acc = acc.concat(firstPage.body.conversations)
-      if (firstPage.body.pages.next) {
-        getMorePages(firstPage.body.pages, acc)
-      }
-      else {
-        mapConvoStats(acc)
-        return acc
-      }
-    }).catch(
-      // Log the rejection reason
-      (reason) => {
-        console.log('Handle rejected promise in list() ('+reason+')');
-      })
-}
-
-// Paginate through Intercom nextPage objects recursively
-function getMorePages(page, acc) {
-  client.nextPage(page).then(
-    function (nextPage) {
-      acc = acc.concat(nextPage.body.conversations)
-      if (nextPage.body.pages.next) {
-        getMorePages(nextPage.body.pages, acc)
-      } else {
-        mapConvoStats(acc)
-        return acc
-      }
-  }).catch(
-      // Log the rejection reason
-      (reason) => {
-        console.log('Handle rejected promise in getMorePages() ('+reason+')');
-      })
-}
-
-// list()
+//function test() {}
 
 // Maps converstation data to simple stats for each team
 function mapConvoStats(data) {
-  const assignees = data.map(function(obj, i) {
-    return obj.assignee;
-  });
-  console.log("🤔", assignees.length);
-  storeStats(assignees, assignees.length)
+  const assignees = data.map(obj => obj.assignee);
+  console.log('🤔', assignees.length);
+  storeStats(assignees, assignees.length);
+}
+
+// Paginate through Intercom nextPage objects recursively
+function getMorePages(page, conversationData) {
+  client.nextPage(page).then(
+    (nextPage) => {
+      const conversationDataMultiple = conversationData.concat(nextPage.body.conversations);
+      if (nextPage.body.pages.next) {
+        getMorePages(nextPage.body.pages, conversationDataMultiple);
+      } else {
+        mapConvoStats(conversationDataMultiple);
+      }
+      return conversationDataMultiple;
+  },
+  ).catch(
+      // Log the rejection reason
+      (reason) => {
+        console.log(`Handle rejected promise in getMorePages() (${reason})`);
+      },
+      );
+}
+
+// Call intercom for first page converations and paginate if more results exist
+function listConversations() {
+  client.conversations.listConversations({ open: true, per_page: 20 }).then(
+    (firstPage, conversationData = []) => {
+      const conversationDataSingle = conversationData.concat(firstPage.body.conversations);
+      if (firstPage.body.pages.next) {
+        getMorePages(firstPage.body.pages, conversationDataSingle);
+      } else {
+        mapConvoStats(conversationDataSingle);
+      }
+      return conversationDataSingle;
+    },
+    ).catch(
+      // Log the rejection reason
+      (reason) => {
+        console.log(`Handle rejected promise in list() (${reason})`);
+      },
+      );
+}
+
+//listConversations();
+
+// When given case ID, get and send all case, customer, and assigned user details to slack
+function caseAttachment(id) {
+  // https://app.intercom.io/a/apps/x2byp8hg/inbox/inbox/1935680/conversations/18437669699
+  // desk.case(id, {}, function(error, data) {
+  // });
+}
+
+// Returns most recent case ids that matches email
+function emailSearch(email) {
+  // desk.get('cases/search/',
+  // {email: email, sort_field:'created_at', sort_direction: 'desc'}, function(error, data) {
+  // });
+}
+
+// Returns a single case attachment using data from
+function caseCard(text, subject, company, etc) {
+  // return `attachement` obect for slack message from data
+}
+
+// Return help text to client with examples of proper usage
+function help(res) {
+  const helpText = 'Type `/support` for status accross all filters. Add a case link `https://app.intercom.io/a/apps/x2byp8hg/inbox/inbox/1935680/conversations/18437669699` or an email `hello@gmail.com` to get specific.'
+  res.send(
+    {
+      response_type: 'ephemeral',
+      text: helpText,
+    },
+  );
 }
 
 // Handler of post requests to server, checks request text to trigger different functions
-app.post('/', function (req, res) {
+app.post('/', (req, res) => {
   // Check the slack token so that this request is authenticated
   if (req.body.token === process.env.SLACK_TOKEN) {
     // Detect which command was entered in slack and call the correct function
     if (req.body.text.length === 0) {
-    
-    }
-    // validates a full Intercom link
-    else if (/^[0-9]{1,7}$/.test(req.body.text.split('conversations/')[1])) {
-      caseAttachment(req.body.text.split('conversations/')[1])
-    } // validates email
-    else if (/([\w\.]+)@([\w\.]+)\.(\w+)/.test(req.body.text)) {
-      emailSearch(req.body.text)
-    } else if (req.body.text === "help") {
-      help()
-    } else if (req.body.text === "test") {
       // getLastStat() should just return a timestamp from the sqlite database table
-      getLastStat.then(function(lastStat) {
-        console.log()
+      getLastStat.then((lastStat) => {
+        console.log();
         res.send(
           {
-            "response_type": "ephemeral",
-            "text": `hello ${ lastStat }`,
-          }
-        )
+            response_type: 'ephemeral',
+            text: `hello ${lastStat}`,
+          },
+        );
       });
+    // validates a full Intercom link exists in command text
+    } else if (/^[0-9]{1,7}$/.test(req.body.text.split('conversations/')[1])) {
+      caseAttachment(req.body.text.split('conversations/')[1]);
+      // validates email
+    } else if (/([\w\.]+)@([\w\.]+)\.(\w+)/.test(req.body.text)) {
+      emailSearch(req.body.text);
+    } else if (req.body.text === 'help') {
+      help(res);
     } else {
       res.send('Sorry bub, I\'m not quite following. Type `/support help` to see what I can understand.');
-    }    
+    }
   } else {
     res.send(
       {
-        "response_type": "ephemeral",
-        "text": "Wow, such unauthorized",
-      }
-    )
+        response_type: 'ephemeral',
+        text: 'Wow, such unauthorized',
+      },
+    );
   }
-  
-  // When given case ID, get and send all case, customer, and assigned user details to slack
-  function caseAttachment(id) {
-    // https://app.intercom.io/a/apps/x2byp8hg/inbox/inbox/1935680/conversations/18437669699
-    // desk.case(id, {}, function(error, data) {
-    // });
-  }
-  
-  // Returns most recent case ids that matches email
-  function emailSearch(email) {
-    // desk.get('cases/search/',{email: email, sort_field:'created_at', sort_direction: 'desc'}, function(error, data) {
-    // });
-  }
-  
-  // Returns a single case attachment using data from 
-  function caseCard(text, subject, company, etc) {
-    // return `attachement` obect for slack message from data
-  }
-  
-  // Return help text with examples
-  function help() {
-    const helpText = "Type `/support` for status accross all filters. Add a case link `https://app.intercom.io/a/apps/x2byp8hg/inbox/inbox/1935680/conversations/18437669699` or an email `hello@gmail.com` to get specific."
-    res.send(
-      {
-        "response_type": "ephemeral",
-        "text": helpText,
-      }
-    )
-  }
-})
+});
 
 // listen for requests :)
-var listener = app.listen(53923, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen(53923, () => {
+  console.log(`Your app is listening on port ${listener.address().port}`);
 });
