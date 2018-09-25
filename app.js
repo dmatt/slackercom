12f74f55470671246149cae856b7626bd3931134
dmatt <dmatt@disqus.com>
2018-09-24 17:17:17 -0700

    Store list of teams in database from Intercom

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-// 👀 Current status: ne-db is working, need to design the records, mapStats gets total count
+// 👀 Current status: properly store status rows on interval (per team monitored) and output
+// last row with slash command
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -22,45 +23,80 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // prevent server from sleeping
 glitchup();
 
-function StatusRecord(timestamp, data, total) {
-  this.timestamp = timestamp;
-  this.data = data;
-  this.total = total;
+class StatusRecord {
+  constructor(type, timestamp, data, total) {
+    this.type = type;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.total = total;
+  }
 }
 
 // default conversations status data
 const defaultStatusRecords = [
-  new StatusRecord('today', { convo: '1' }, 1),
-  new StatusRecord('yesterday', { convo: '2' }, 2),
+  new StatusRecord('status', 'today', { convo: '1' }, 1),
+  new StatusRecord('status', 'yesterday', { convo: '2' }, 2),
 ];
 
-db.count({}, (countErr, count) => {
-  console.log(`There are ${count} conversation rows in the database`);
+// Initialize database with Teams
+db.count({ type: 'team' }, (countErr, count) => {
+  console.log(`There are ${count} team rows in the database`);
+  if (countErr) console.log('There\'s a problem with the database: ', countErr);
+  else if (count <= 0) { // empty database so needs populating
+    // default users inserted in the database
+    storeTeams();
+  }
+});
+
+// Initialize database with Conversation stats
+db.count({type: 'status'}, (countErr, count) => {
+  console.log(`There are ${count} status rows in the database`);
   if (countErr) console.log('There\'s a problem with the database: ', countErr);
   else if (count <= 0) { // empty database so needs populating
     // default users inserted in the database
     db.insert(defaultStatusRecords, (insertErr, recordsAdded) => {
       if (insertErr) console.log('There\'s a problem with the database: ', insertErr);
-      else if (recordsAdded) console.log('Default recordsAdded inserted in the database');
+      else if (recordsAdded) console.log('Default statuses inserted in the database');
     });
   }
 });
 
 // Array of team name strings to monitor, default is all teams
-let monitoredTeams = []
+let monitoredTeams = [];
 
 // Callback to list() on interval get Intercom data
 // setInterval(list, 1000 * 60 * 10 );
 // setInterval(list, 3000 );
 
-// Call intercom for first page converations and paginate if more results exist
-//function test() {}
+function failureCallback(result) {
+  console.log(`Handle rejected promise in listTeams() (${result})`);
+}
+
+// Call intercom for all admins, which includes teams
+function storeTeams() {
+  client.admins.list().then(
+    (firstPage) => {
+      const teams = firstPage.body.admins.filter(admin => admin.type === 'team');
+      console.log(teams);
+      return teams;
+    },
+    ).then(
+      (teams) => {
+        teams.forEach((team) => {
+          db.insert(team, (insertErr, recordsAdded) => {
+            if (insertErr) console.log('There\'s a problem with the database: ', insertErr);
+            else if (recordsAdded) console.log('Team inserted in the database');
+          });
+        });
+      },
+      ).catch(failureCallback);
+    }
 
 // Maps converstation data to simple stats for each team
 function mapConvoStats(data) {
   const assignees = data.map(obj => obj.assignee);
   console.log('🤔', assignees.length);
-  storeStats(assignees, assignees.length);
+  // storeStats(assignees, assignees.length);
 }
 
 // Paginate through Intercom nextPage objects recursively
@@ -75,17 +111,12 @@ function getMorePages(page, conversationData) {
       }
       return conversationDataMultiple;
   },
-  ).catch(
-      // Log the rejection reason
-      (reason) => {
-        console.log(`Handle rejected promise in getMorePages() (${reason})`);
-      },
-      );
+  ).catch(failureCallback);
 }
 
 // Call intercom for first page converations and paginate if more results exist
 function listConversations() {
-  client.conversations.listConversations({ open: true, per_page: 20 }).then(
+  client.conversations.list({ open: true, per_page: 20 }).then(
     (firstPage, conversationData = []) => {
       const conversationDataSingle = conversationData.concat(firstPage.body.conversations);
       if (firstPage.body.pages.next) {
@@ -95,15 +126,10 @@ function listConversations() {
       }
       return conversationDataSingle;
     },
-    ).catch(
-      // Log the rejection reason
-      (reason) => {
-        console.log(`Handle rejected promise in list() (${reason})`);
-      },
-      );
+    ).catch(failureCallback);
 }
 
-//listConversations();
+// listConversations();
 
 // When given case ID, get and send all case, customer, and assigned user details to slack
 function caseAttachment(id) {
@@ -126,7 +152,7 @@ function caseCard(text, subject, company, etc) {
 
 // Return help text to client with examples of proper usage
 function help(res) {
-  const helpText = 'Type `/support` for status accross all filters. Add a case link `https://app.intercom.io/a/apps/x2byp8hg/inbox/inbox/1935680/conversations/18437669699` or an email `hello@gmail.com` to get specific.'
+  const helpText = 'Type `/support` for status accross all filters. Add a case link `https://app.intercom.io/a/apps/x2byp8hg/inbox/inbox/1935680/conversations/18437669699` or an email `hello@gmail.com` to get specific.';
   res.send(
     {
       response_type: 'ephemeral',
