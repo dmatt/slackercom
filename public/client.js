e22f5fd4398272e47638836b3337ebf2b3bf03b5
dmatt <dmatt@disqus.com>
2019-03-04 17:27:39 -0800

    Try multi-line graph example by Curran Kelleher

--- public/client.js
+++ public/client.js
@@ -1,35 +1,32 @@
 // client-side js
 // run by the browser each time your view template referencing it is loaded
+/* global makeGraph */
+/* eslint-env browser */
 
 console.log('hello world :o');
 
 let dataSet = [];
 
-// define variables that reference elements on our page
+// a helper function that creates a list item for a given dream
+const appendNewStatus = (status) => {
+  const newListItem = document.createElement('li');
+  newListItem.innerHTML = status;
+};
 
-// a helper function to call when our request for dreams is done
-const getStatusListener = function() {
+// a helper function to call when our request for statusRequest is done
+function getStatusListener() {
   // parse our response to convert to JSON
   dataSet = JSON.parse(this.responseText);
   makeGraph(dataSet);
 
   // iterate through every dream and add it to our page
-  dataSet.forEach( function(row) {
+  dataSet.forEach((row) => {
     appendNewStatus(JSON.stringify(row));
   });
 }
 
-// request the dreams from our app's sqlite database
+// request the statusRequest from our app's sqlite database
 const statusRequest = new XMLHttpRequest();
 statusRequest.onload = getStatusListener;
 statusRequest.open('get', '/getStatus');
 statusRequest.send();
-
-// a helper function that creates a list item for a given dream
-const appendNewStatus = function(status) {
-  const newListItem = document.createElement('li');
-  newListItem.innerHTML = status;
-
-}
-
-
--- public/graph-mvp.js
+++ public/graph-mvp.js
@@ -1,42 +1,146 @@
+/* global d3 */
+/* eslint-env browser */
 // maybe: https://beta.observablehq.com/@mbostock/d3-multi-line-chart
 
 // var dataSet = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 
-let makeGraph = (dataSet) => {
-  console.log(dataSet)
-  d3.select(".chart")
-    .selectAll("div")
-    .data(dataSet)
-      .enter()
-      .append("div")
-      .style("width", function(d) { return d.data["Community Publishers"]**3 + "px"; })
-      .text(function(d) { return d.data["Community Publishers"]; });
-}
+// Callback from client.js
+const makeGraph = (dataSet) => {
 
-/*
+            const svg = d3.select('svg');
 
-  var svgWidth = 500, svgHeight = 300, barPadding = 1;
-  var barWidth = (svgWidth / dataSet.length);
+            const width = +svg.attr('width');
+            const height = +svg.attr('height');
 
-  var svg = d3.select('svg')
-      .attr("width", svgWidth)
-      .attr("height", svgHeight);
+            const render = (data) => {
+                const title = 'Team Counts over time';
 
-  var barChart = svg.selectAll("rect")
-      .data(dataSet)
-      .enter()
-      .append("rect")
-      .attr("y", function(d) {
-          return svgHeight - d.data["Community Publishers"]
+                const xValue = d => d.timestamp;
+                const xAxisLabel = 'Time';
+                
+                const yValue = d => d.count;
+                const circleRadius = 6;
+                const yAxisLabel = 'Count';
+                
+                const colorValue = d => d.team;
+
+                const margin = {
+                    top: 60, right: 160, bottom: 88, left: 105,
+                };
+                const innerWidth = width - margin.left - margin.right;
+                const innerHeight = height - margin.top - margin.bottom;
+
+                const xScale = d3.scaleTime()
+                    .domain(d3.extent(data, xValue))
+                    .range([0, innerWidth])
+                    .nice();
+
+                const yScale = d3.scaleLinear()
+                    .domain(d3.extent(data, yValue))
+                    .range([innerHeight, 0])
+                    .nice();
+
+                const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+                const g = svg.append('g')
+                    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+                const xAxis = d3.axisBottom(xScale)
+                    .tickSize(-innerHeight)
+                    .tickPadding(15);
+
+                const yAxis = d3.axisLeft(yScale)
+                    .tickSize(-innerWidth)
+                    .tickPadding(10);
+
+                const yAxisG = g.append('g').call(yAxis);
+                yAxisG.selectAll('.domain').remove();
+
+                yAxisG.append('text')
+                    .attr('class', 'axis-label')
+                    .attr('y', -60)
+                    .attr('x', -innerHeight / 2)
+                    .attr('fill', 'black')
+                    .attr('transform', 'rotate(-90)')
+                    .attr('text-anchor', 'middle')
+                    .text(yAxisLabel);
+
+                const xAxisG = g.append('g').call(xAxis)
+                    .attr('transform', `translate(0, ${innerHeight})`);
+
+                xAxisG.select('.domain').remove();
+
+                xAxisG.append('text')
+                    .attr('class', 'axis-label')
+                    .attr('y', 80)
+                    .attr('x', innerWidth / 2)
+                    .attr('fill', 'black')
+                    .text(xAxisLabel);
+
+                const lineGenerator = d3.line()
+                    .x(d => xScale(xValue(d)))
+                    .y(d => yScale(yValue(d)))
+                    .curve(d3.curveBasis);
+
+                const lastYValue = d => yValue(d.values[d.values.length - 1]);
+   
+                const nested = d3.nest()
+                .key(colorValue)
+                .entries(data)
+                .sort((a, b) => d3.descending(lastYValue(a), lastYValue(b)));
+
+                console.log(nested);
+
+                colorScale.domain(nested.map(d => d.key));
+
+                g.selectAll('.line-path').data(nested)
+                    .enter().append('path')
+                    .attr('class', 'line-path')
+                    .attr('d', d => lineGenerator(d.values))
+                    .attr('stroke', d => colorScale(d.key));
+
+                g.append('text')
+                    .attr('class', 'title')
+                    .attr('y', -10)
+                    .text(title);
+
+                svg.append('g')
+                    .attr('transform', `translate(790,121)`)
+                    .call(colorLegend, {
+                      colorScale,
+                      circleRadius: 13,
+                      spacing: 30,
+                      textOffset: 15
+                    });
+            };
+
+    // add team attribute to status counts so that d3.nest() can use it
+    const explodeByTeam = d => d.map(status => (
+        Object.keys(status.data).map(team => ({
+            type: status.type,
+            timestamp: status.timestamp,
+            team,
+            count: status.data[team],
+        }))
+        ));
+
+/*         d3.csv('https://vizhub.com/curran/datasets/data-canvas-sense-your-city-one-week.csv')
+        .then(data => {
+          data.forEach(d => {
+            d.temperature = +d.temperature;
+            d.timestamp = new Date(d.timestamp);
+          });
+          render(data);
+        }); */
+
+        // get the exploded by team data and flatten the array of arrays to 1 level
+        let data = explodeByTeam(dataSet);
+        data = data.flat();
+        console.log(data);
         
-      })
-      .attr("height", function(d) {
-          return d.data["Community Publishers"]
-      })
-      .attr("width", barWidth - barPadding)
-      .attr("transform", function (d, i) {
-          var translate = [barWidth * i, 0]; 
-          return "translate("+ translate +")";
-      });
-
-*/
+        data.forEach((d) => {
+            d.count = +d.count;
+            d.timestamp = new Date(d.timestamp);
+        });
+        render(data);
+    };
